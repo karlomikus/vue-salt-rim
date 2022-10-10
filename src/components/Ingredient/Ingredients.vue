400d296e831c22e6a51ba7beadd3a329c3b22779
Karlo Mikuš <karlo.mikus1@gmail.com>
2022-10-10 23:16:57 +0200

    Update with slug

--- src/components/Ingredient/Ingredients.vue
+++ src/components/Ingredient/Ingredients.vue
@@ -18,7 +18,7 @@ import Spinner from './../Spinner.vue'
                 <div class="ingredient-list__description">
                     <h3>{{ ingredient.name }}</h3>
                     <p>{{ ingredient.description }}</p>
-                    <RouterLink :to="{name: 'ingredients.show', params: {id: ingredient.id}}" class="button-more">
+                    <RouterLink :to="{name: 'ingredients.show', params: {id: ingredient.slug}}" class="button-more">
                         <span>Learn more</span>
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20">
                             <path fill="none" d="M0 0h24v24H0z" />
