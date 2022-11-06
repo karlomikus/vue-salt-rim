9f3aba1b3d85ced4707ebe015c9a0b5448ddbe7c
Karlo Mikuš <karlo.mikus1@gmail.com>
2022-11-06 14:24:08 +0100

    Make docker image smaller

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,6 @@ RUNTIME_ENV_CONFIG="
     window.srConfig.API_URL = \"$API_URL\";
     window.srConfig.MEILISEARCH_HOST = \"$MEILISEARCH_HOST\";
 "
-echo $RUNTIME_ENV_CONFIG > dist/config.js
-echo $RUNTIME_ENV_CONFIG > public/config.js
+echo $RUNTIME_ENV_CONFIG > /app/dist/config.js
 
-exec http-server dist/ -c-1 --proxy http://localhost:8080?
+exec http-server /app/dist/ -c-1 --proxy http://localhost:8080?
