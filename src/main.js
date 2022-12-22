a2d71d2b95ea06acdb556898d6ad8dc81b86acd2
Karlo Mikuš <karlo.mikus1@gmail.com>
2022-12-22 19:41:05 +0100

    Bump version

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import './assets/main.css'
 
 const app = createApp(App)
 
-app.config.globalProperties.app_version = 'v1.0.0';
+app.config.globalProperties.app_version = 'v1.1.0';
 
 app.use(router)
 app.use(InstantSearch)
