f023e1a7879cc5270f7f9075531a0610b872a6ef
Karlo Mikuš <karlo.mikus1@gmail.com>
2023-01-05 18:31:55 +0100

    Add PWA settings

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,32 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
+
+const manifest = {
+  name: 'Salt Rim for Bar Assistant',
+  short_name: 'Salt Rim',
+  description: 'Bar assistant is a self hosted application for managing your home bar.',
+  theme_color: '#737db7',
+  icons: [
+    {
+      src: 'pwa-192.png',
+      sizes: '192x192',
+      type: 'image/png'
+    },
+    {
+      src: 'pwa-512.png',
+      sizes: '512x512',
+      type: 'image/png'
+    }
+  ]
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), VitePWA({
+    manifest: manifest
+  })],
   envDir: './',
   resolve: {
     alias: {
