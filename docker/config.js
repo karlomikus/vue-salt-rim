1a71d64427200a2b039c20cb59cf2e6e1088b9e9
Dan Simpson <daniel_simpson1988@hotmail.com>
2023-03-20 17:26:00 +1100

    Add bar name and description parameters from docker env variables

--- docker/config.js
+++ docker/config.js
@@ -1,3 +1,5 @@
 window.srConfig = {}
 window.srConfig.API_URL = "$API_URL"
-window.srConfig.MEILISEARCH_URL = "$MEILISEARCH_URL"
+window.srConfig.MEILISEARCH_URL = "$MEILISEARCH_URL";
+window.srConfig.BAR_NAME = "$BAR_NAME";
+window.srConfig.DESCRIPTION = "$DESCRIPTION";
