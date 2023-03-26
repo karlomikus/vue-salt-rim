593d3552c1c89ba6800d1b97bc4c889475019407
Karlo Mikuš <karlo.mikus1@gmail.com>
2023-03-26 20:40:57 +0200

    Add default locale #69

--- docker/config.js
+++ docker/config.js
@@ -3,3 +3,4 @@ window.srConfig.API_URL = "$API_URL"
 window.srConfig.MEILISEARCH_URL = "$MEILISEARCH_URL";
 window.srConfig.BAR_NAME = "$BAR_NAME";
 window.srConfig.BAR_DESCRIPTION = "$DESCRIPTION";
+window.srConfig.DEFAULT_LOCALE = "$DEFAULT_LOCALE";
