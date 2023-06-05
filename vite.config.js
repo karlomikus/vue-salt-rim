74e4470e4342c0b0bc80d0a7616882266b997d36
Karlo Mikuš <karlo.mikus1@gmail.com>
2023-06-05 21:53:07 +0200

    Ignore specific routes from pwa

--- vite.config.js
+++ vite.config.js
@@ -25,6 +25,9 @@ const manifest = {
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue(), VitePWA({
+        workbox: {
+            navigateFallbackDenylist: [/^\/bar/]
+        },
         manifest: manifest,
         registerType: 'autoUpdate',
     })],
