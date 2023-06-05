35a10e2697cabba893dba99c18b2255e4abedb13
Karlo Mikuš <karlo.mikus1@gmail.com>
2023-06-05 17:16:57 +0200

    Add image share

---
+++ src/components/Cocktail/ImageDialog.vue
@@ -0,0 +1,91 @@
+<template>
+    <div>
+        <OverlayLoader v-if="isLoading" />
+        <div class="dialog-title">{{ $t('generate-image-dialog.preview') }}</div>
+        <div class="image-download-preview">
+            <img v-if="imagePayload" :src="imagePayload" alt="">
+            <div v-else class="image-export-wrapper" ref="exportElement">
+                <PublicRecipe :cocktail="cocktail" :currentUnit="currentUnit" :hideUnits="true"></PublicRecipe>
+            </div>
+        </div>
+        <div class="dialog-actions" style="margin-top: 1rem;">
+            <button type="button" class="button button--outline" @click="$emit('publicDialogClosed')">{{ $t('cancel') }}</button>
+            <button v-if="shareEnabled" type="button" class="button button--outline" @click="shareAction">{{ $t('share') }}</button>
+            <a v-if="imagePayload" :href="imagePayload" :download="fileName" class="button button--dark">{{ $t('download') }}</a>
+        </div>
+    </div>
+</template>
+
+<script>
+import OverlayLoader from '@/components/OverlayLoader.vue'
+import PublicRecipe from '@/components/Cocktail/PublicRecipe.vue'
+import * as htmlToImage from 'html-to-image';
+
+export default {
+    props: ['cocktail'],
+    data() {
+        return {
+            isLoading: false,
+            imagePayload: null,
+            currentUnit: 'ml',
+            shareEnabled: false,
+        }
+    },
+    components: {
+        OverlayLoader,
+        PublicRecipe
+    },
+    computed: {
+        fileName() {
+            return this.cocktail.slug + '.png';
+        }
+    },
+    mounted() {
+        if (localStorage.getItem('defaultUnit')) {
+            this.currentUnit = localStorage.getItem('defaultUnit')
+        }
+
+        if ("share" in navigator) {
+            this.shareEnabled = true;
+        }
+
+        this.downloadImage();
+    },
+    methods: {
+        downloadImage() {
+            this.isLoading = true;
+            htmlToImage.toPng(this.$refs.exportElement, {
+                cacheBust: true
+            }).then((dataUrl) => {
+                this.isLoading = false;
+                this.imagePayload = dataUrl;
+            }).catch((e) => {
+                this.isLoading = false;
+                console.error(e)
+                this.$toast.error(this.$t('generate-image-dialog.generation-failed'));
+            });
+        },
+        async shareAction() {
+            const blobData = await (await fetch(this.imagePayload)).blob()
+            const file = new File([blobData], this.fileName, {type: blobData.type})
+
+            try {
+                await navigator.share({
+                    title: this.cocktail.name,
+                    text: this.cocktail.description,
+                    files: [file]
+                });
+            } catch (err) {
+                console.error(err)
+            }
+        }
+    }
+};
+</script>
+
+<style scoped>
+.image-download-preview {
+    overflow: scroll;
+    max-height: 350px;
+}
+</style>
---
+++ src/components/Cocktail/PublicRecipe.vue
@@ -0,0 +1,229 @@
+<template>
+    <div class="public-cocktail-recipe" ref="recipe">
+        <div class="public-cocktail-recipe__image" :style="{'background-image': 'url(' + mainImage.url + ')'}">
+            <div class="public-cocktail-recipe__image__copyright" v-if="mainImage.copyright">Image &copy; {{ mainImage.copyright }}</div>
+        </div>
+        <div class="public-cocktail-recipe__summary">
+            <h2>{{ cocktail.name }}</h2>
+            <div v-show="!!cocktail.description" class="public-cocktail-recipe__content" v-html="parsedDescription"></div>
+            <div class="public-cocktail-recipe__units" v-show="hideUnits == false">
+                <button type="button" class="button button--public" :class="{'button--active': currentUnit == 'ml'}" @click="currentUnit = 'ml'">ml</button>
+                <button type="button" class="button button--public" :class="{'button--active': currentUnit == 'oz'}" @click="currentUnit = 'oz'">oz</button>
+                <button type="button" class="button button--public" :class="{'button--active': currentUnit == 'cl'}" @click="currentUnit = 'cl'">cl</button>
+            </div>
+            <div class="public-cocktail-recipe__summary__section">
+                <h3>Ingredients</h3>
+                <ul>
+                    <li v-for="ing in cocktail.ingredients">
+                        <span>{{ ing.name }}</span><span class="spacer"></span><span class="amount-units">{{ parseIngredientAmount(ing) }}</span>
+                    </li>
+                </ul>
+            </div>
+            <div class="public-cocktail-recipe__summary__section">
+                <h3>Instructions</h3>
+                <div class="public-cocktail-recipe__content" v-html="parsedInstructions"></div>
+            </div>
+            <div class="public-cocktail-recipe__summary__section" v-show="!!cocktail.garnish">
+                <h3>Garnish</h3>
+                <div class="public-cocktail-recipe__content" v-html="parsedGarnish"></div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { marked } from 'marked';
+import Utils from '@/Utils';
+
+export default {
+    props: {
+        cocktail: {
+            type: Object,
+            default: {}
+        },
+        currentUnit: {
+            type: String,
+            default: 'ml'
+        },
+        hideUnits: {
+            type: Boolean,
+            default: false
+        }
+    },
+    data() {
+        return {
+            isLoading: false
+        }
+    },
+    computed: {
+        mainImage() {
+            if (this.cocktail && this.cocktail.images) {
+                return this.cocktail.images[0]
+            }
+
+            return {};
+        },
+        parsedDescription() {
+            if (!this.cocktail.description) {
+                return null;
+            }
+
+            return marked.parse(this.cocktail.description)
+        },
+        parsedInstructions() {
+            if (!this.cocktail.instructions) {
+                return null;
+            }
+
+            return marked.parse(this.cocktail.instructions)
+        },
+        parsedGarnish() {
+            if (!this.cocktail.garnish) {
+                return null;
+            }
+
+            return marked.parse(this.cocktail.garnish)
+        },
+    },
+    methods: {
+        parseIngredientAmount(ingredient) {
+            return Utils.printIngredientAmount(ingredient, this.currentUnit);
+        },
+    }
+}
+</script>
+
+<style scoped>
+.public-cocktail-recipe {
+    --clr-content-bg: var(--clr-gray-50);
+    --clr-content: var(--clr-gray-600);
+    --clr-marker: var(--clr-red-800);
+    --shadow-color: 239deg 26% 22%;
+    --shadow-elevation-medium:
+        0px 0.8px 0.9px hsl(var(--shadow-color) / 0.28),
+        -0.1px 2.6px 3.1px -0.7px hsl(var(--shadow-color) / 0.3),
+        -0.1px 6.3px 7.4px -1.4px hsl(var(--shadow-color) / 0.32),
+        -0.3px 14.9px 17.5px -2.1px hsl(var(--shadow-color) / 0.35);
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    place-content: center;
+    margin-inline: auto;
+    background: var(--clr-content-bg);
+    border-radius: 1rem;
+    overflow: hidden;
+    box-shadow: var(--shadow-elevation-medium);
+    align-items: start;
+}
+
+.dark-theme .public-cocktail-recipe {
+    --clr-content-bg: linear-gradient(160deg, var(--clr-dark-main-900) -15%, var(--clr-dark-main-950) 80%);
+    --clr-content: var(--clr-gray-200);
+    --shadow-color: 239deg 15% 5%;
+    --clr-marker: var(--clr-red-400);
+    border-top: 1px solid var(--clr-dark-main-800);
+}
+
+@media (max-width: 700px) {
+    .public-cocktail-recipe {
+        grid-template-columns: 1fr;
+    }
+}
+
+.public-cocktail-recipe__image {
+    position: relative;
+    background-position: center;
+    background-size: cover;
+    height: 100%;
+    min-height: 600px;
+}
+
+.public-cocktail-recipe__image__copyright {
+    position: absolute;
+    top: 1rem;
+    left: 1rem;
+    display: inline-block;
+    background-color: rgba(0, 0, 0, .4);
+    color: #fff;
+    border-radius: 15px;
+    padding: 2px 7px;
+    font-size: 0.7rem;
+    z-index: 2;
+}
+
+.public-cocktail-recipe h2 {
+    font-size: 3rem;
+    line-height: 1.2;
+    font-family: var(--font-heading);
+}
+
+.public-cocktail-recipe h3 {
+    font-size: 0.75rem;
+    letter-spacing: 1px;
+    font-weight: var(--fw-bold);
+    color: var(--clr-gray-400);
+    text-transform: uppercase;
+    flex-basis: 120px;
+    flex-shrink: 0;
+    line-height: 2;
+}
+
+.public-cocktail-recipe__summary {
+    padding: 2rem;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+}
+
+.public-cocktail-recipe__summary__section {
+    display: flex;
+    flex-direction: row;
+}
+
+@media (max-width: 450px) {
+    .public-cocktail-recipe h3 {
+        flex-basis: 100%;
+    }
+    .public-cocktail-recipe__summary__section {
+        flex-direction: column;
+    }
+}
+
+.public-cocktail-recipe__content {
+    color: var(--clr-content);
+}
+
+:deep(.public-cocktail-recipe__summary ol) {
+    padding-left: 18px;
+    color: var(--clr-content);
+    width: 100%;
+}
+
+:deep(.public-cocktail-recipe__summary ol li::marker) {
+    color: var(--clr-marker);
+}
+
+.public-cocktail-recipe__summary ul {
+    padding: 0;
+    width: 100%;
+}
+
+.public-cocktail-recipe__summary ul li {
+    display: flex;
+    justify-content: space-between;
+    align-items: baseline;
+}
+
+.public-cocktail-recipe__summary ul li .spacer {
+    flex-grow: 1;
+    border-bottom: 1px dotted var(--clr-gray-300);
+    margin: 0 0.5rem;
+}
+
+.public-cocktail-recipe__summary ul li .amount-units {
+    font-weight: var(--fw-bold);
+}
+
+.public-cocktail-recipe__units {
+    text-align: right;
+}
+</style>
