ceaa564a348ae35d5a2b9eeb040353c44daed799
Karlo Mikuš <karlo.mikus1@gmail.com>
2023-07-31 20:01:13 +0200

    Update pagination styling

--- src/components/Cocktail/CocktailServerList.vue
+++ src/components/Cocktail/CocktailServerList.vue
@@ -90,7 +90,13 @@
                         {{ $t('pagination.results', {'on_page_results': meta.to || 0, total_results: meta.total}) }}
                     </div>
                     <div class="resource-search__content__pagination__links">
-                        <button type="button" class="button button--input" @click="changePage('prev')"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path d="M10.8284 12.0007L15.7782 16.9504L14.364 18.3646L8 12.0007L14.364 5.63672L15.7782 7.05093L10.8284 12.0007Z"></path></svg></button> {{ $t('page') }} {{ meta.current_page }}/{{ meta.last_page }} <button type="button" class="button button--input" @click="changePage('next')"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path d="M13.1714 12.0007L8.22168 7.05093L9.63589 5.63672L15.9999 12.0007L9.63589 18.3646L8.22168 16.9504L13.1714 12.0007Z"></path></svg></button>
+                        <button type="button" class="button button--outline button--icon" @click="changePage('prev')">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path d="M10.8284 12.0007L15.7782 16.9504L14.364 18.3646L8 12.0007L14.364 5.63672L15.7782 7.05093L10.8284 12.0007Z"></path></svg>
+                        </button>
+                        {{ $t('page') }} {{ meta.current_page }}/{{ meta.last_page }}
+                        <button type="button" class="button button--outline button--icon" @click="changePage('next')">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path d="M13.1714 12.0007L8.22168 7.05093L9.63589 5.63672L15.9999 12.0007L9.63589 18.3646L8.22168 16.9504L13.1714 12.0007Z"></path></svg>
+                        </button>
                     </div>
                 </div>
             </div>
@@ -470,6 +476,9 @@ export default {
 
 .resource-search__content__pagination__links {
     margin-left: auto;
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
 }
 
 @media (max-width: 750px) {
