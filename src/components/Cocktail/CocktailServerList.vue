25c3ec2ce609747256d950eb762b6814b0b138b0
Karlo Mikuš <karlo.mikus1@gmail.com>
2023-08-01 17:37:18 +0200

    Move ingredients to api requests

--- src/components/Cocktail/CocktailServerList.vue
+++ src/components/Cocktail/CocktailServerList.vue
@@ -85,20 +85,7 @@
                     </svg>
                     <p>{{ $t('cocktails-not-found') }}</p>
                 </div>
-                <div class="resource-search__content__pagination">
-                    <div class="resource-search__content__pagination__page_info">
-                        {{ $t('pagination.results', {'on_page_results': meta.to || 0, total_results: meta.total}) }}
-                    </div>
-                    <div class="resource-search__content__pagination__links">
-                        <button type="button" class="button button--outline button--icon" @click="changePage('prev')">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path d="M10.8284 12.0007L15.7782 16.9504L14.364 18.3646L8 12.0007L14.364 5.63672L15.7782 7.05093L10.8284 12.0007Z"></path></svg>
-                        </button>
-                        {{ $t('page') }} {{ meta.current_page }}/{{ meta.last_page }}
-                        <button type="button" class="button button--outline button--icon" @click="changePage('next')">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path d="M13.1714 12.0007L8.22168 7.05093L9.63589 5.63672L15.9999 12.0007L9.63589 18.3646L8.22168 16.9504L13.1714 12.0007Z"></path></svg>
-                        </button>
-                    </div>
-                </div>
+                <Pagination :meta="meta" @pageChanged="handlePageChange"></Pagination>
             </div>
         </div>
     </div>
@@ -113,6 +100,7 @@ import CocktailGridContainer from './CocktailGridContainer.vue'
 import PageHeader from './../PageHeader.vue'
 import Auth from './../../Auth.js';
 import Refinement from './../Search/Refinement.vue';
+import Pagination from './../Search/Pagination.vue';
 import CollectionDialog from './../Cocktail/CollectionDialog.vue';
 import Dialog from './../Dialog/Dialog.vue'
 import qs from 'qs';
@@ -173,7 +161,8 @@ export default {
         OverlayLoader,
         Refinement,
         Dialog,
-        CollectionDialog
+        CollectionDialog,
+        Pagination
     },
     created() {
         document.title = `${this.$t('cocktails')} \u22C5 ${this.site_title}`
@@ -316,19 +305,8 @@ export default {
                 this.isLoading = false;
             })
         },
-        changePage(dir) {
-            if (dir == 'prev') {
-                if (this.currentPage > 1) {
-                    this.currentPage--;
-                }
-            }
-
-            if (dir == 'next') {
-                if (this.currentPage < this.meta.last_page) {
-                    this.currentPage++;
-                }
-            }
-
+        handlePageChange(toPage) {
+            this.currentPage = toPage
             this.updateRouterPath()
         },
         queryToState() {
