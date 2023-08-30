25d72ed5dec5b4c7e8277d753e93a4810dd1ec3e
Karlo Mikuš <karlo.mikus1@gmail.com>
2023-08-30 11:21:24 +0200

    Update rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,10 @@ module.exports = {
     'plugin:vue/vue3-strongly-recommended',
   ],
   rules: {
-    "vue/html-indent": ["warn", 4]
+    "vue/html-indent": ["warn", 4],
+    "vue/max-attributes-per-line": ['off'],
+    "vue/multiline-html-element-content-newline": ['off'],
+    "vue/html-self-closing": ['off'],
+    "vue/singleline-html-element-content-newline": ['off'],
   }
 }
