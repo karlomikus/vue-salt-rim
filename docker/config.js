e3bce0294f026b810b602c32fcb7edab47d2a5a0
Karlo Mikuš <karlo.mikus1@gmail.com>
2023-12-13 21:06:00 +0100

    Add missing env vars

--- docker/config.js
+++ docker/config.js
@@ -5,3 +5,5 @@ window.srConfig.MEILISEARCH_URL = "$MEILISEARCH_URL";
 window.srConfig.DEFAULT_LOCALE = "$DEFAULT_LOCALE";
 window.srConfig.ENV = "$SALT_RIM_ENV";
 window.srConfig.MAILS_ENABLED = "$MAILS_ENABLED" === 'true';
+window.srConfig.BILLING_ENABLED = "$BILLING_ENABLED" === 'true';
+window.srConfig.BILLING_ENV = "$BILLING_ENV";
