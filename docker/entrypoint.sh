c4cd40937c8029eb73653df318e9b78e473ed921
Karlo Mikuš <karlo.mikus1@gmail.com>
2023-12-21 17:19:13 +0100

    Update placeholder string

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,6 +4,6 @@ envsubst < /var/www/config.js > /var/www/html/config.js
 
 replacement = "$ANALYTICS_SCRIPT"
 
-sed -i "s/<!-- ANALYTICS -->/$replacement/g" /var/www/html/index.html
+sed -i "s~{script}~$replacement~g" /var/www/html/index.html
 
 exec nginx -g "daemon off;"
