2a1213fecad7c181c69d5ec64ce34944fd91eadc
Karlo Mikuš <karlo.mikus1@gmail.com>
2023-12-21 17:00:45 +0100

    Add analytics script env

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,4 +2,8 @@
 
 envsubst < /var/www/config.js > /var/www/html/config.js
 
+replacement = "$ANALYTICS_SCRIPT"
+
+sed -i "s/<!-- ANALYTICS -->/$replacement/g" /var/www/html/index.html
+
 exec nginx -g "daemon off;"
