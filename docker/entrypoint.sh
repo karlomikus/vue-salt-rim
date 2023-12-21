b2e5920f0ef439f55e69b7347ccccfb30502a240
Karlo Mikuš <karlo.mikus1@gmail.com>
2023-12-21 17:35:46 +0100

    Remove placeholder

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,8 +2,4 @@
 
 envsubst < /var/www/config.js > /var/www/html/config.js
 
-replacement = "$ANALYTICS_SCRIPT"
-
-sed -i "s~{script}~$replacement~g" /var/www/html/index.html
-
 exec nginx -g "daemon off;"
