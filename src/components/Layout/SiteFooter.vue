549476065236f564f57dd5430b711b505280b9da
Karlo Mikuš <karlo.mikus1@gmail.com>
2023-12-28 16:29:51 +0100

    Update footer

--- src/components/Layout/SiteFooter.vue
+++ src/components/Layout/SiteFooter.vue
@@ -2,30 +2,28 @@
     <footer class="site-footer-wrapper">
         <div class="site-footer">
             <div class="site-footer__links">
-                <a target="_blank" href="https://barassistant.app">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 512 512" width="20" height="20">
-                        <path d="M512 0H0v512h512V0Z" />
-                        <path fill="#fff" d="M420.866 228.925h-117.76l-204.8 143.811h-46.08V49.152h46.08l204.8 143.811h117.76v-89.877h46.08v215.716h-46.08v-89.877Zm-276.48 63.099v-162.16l-46.08-32.359v226.878l46.08-32.359ZM256.003 429.056H44.035v48.128h211.968v-48.128Z" />
-                    </svg>
-                    Bar Assistant
-                </a>
+                <a target="_blank" href="https://barassistant.app">Bar Assistant</a>
+                <span>&middot;</span>
+                <a target="_blank" href="https://bar-assistant.github.io/docs/">Docs</a>
+                <span>&middot;</span>
+                <a target="_blank" href="https://github.com/bar-assistant">GitHub</a>
                 <template v-if="appState.isBillingEnabled()">
-                    &middot;
+                    <span>&middot;</span>
                     <a target="_blank" href="https://barassistant.app/terms.html">
                         Terms
                     </a>
-                    &middot;
+                    <span>&middot;</span>
                     <a target="_blank" href="https://barassistant.app/privacy.html">
                         Privacy policy
                     </a>
                 </template>
             </div>
-            <div class="site-footer__versions">
-                {{ clientVersion }}
-                &middot;
-                {{ apiVersion }}
-                &middot;
-                {{ meiliVersion }}
+            <div class="site-footer__links">
+                <span>Salt Rim: <a href="https://github.com/karlomikus/vue-salt-rim/releases" target="_blank">{{ clientVersion }}</a></span>
+                <span>&middot;</span>
+                <span>Bar Assistant: <a href="https://github.com/karlomikus/bar-assistant/releases" target="_blank">{{ apiVersion }}</a></span>
+                <span>&middot;</span>
+                <span>Meilisearch: <a href="https://github.com/meilisearch/meilisearch/releases" target="_blank">{{ meiliVersion }}</a></span>
             </div>
         </div>
     </footer>
@@ -48,13 +46,13 @@ export default {
     },
     computed: {
         clientVersion() {
-            return `Salt Rim: ${this.versions.client}`
+            return `${this.versions.client}`
         },
         apiVersion() {
-            return `Bar Assistant: ${this.versions.api}`
+            return `${this.versions.api}`
         },
         meiliVersion() {
-            return `Meilisearch: ${this.versions.meili}`
+            return `${this.versions.meili}`
         }
     },
     created() {
@@ -98,27 +96,16 @@ export default {
     display: flex;
     gap: 0.5rem;
     flex-direction: row;
+    flex-wrap: wrap;
 }
 
-.site-footer__links a {
-    display: inline-flex;
-    text-decoration: none;
+.site-footer a {
     color: var(--clr-footer-link);
 }
 
-.site-footer__links a svg {
-    margin-right: 5px;
-    fill: var(--clr-footer-link);
-}
-
-.site-footer__links a:hover svg {
-    margin-right: 5px;
-    fill: var(--clr-footer-link-hover);
-}
-
-.site-footer__links a:hover,
-.site-footer__links a:active,
-.site-footer__links a:focus {
+.site-footer a:hover,
+.site-footer a:active,
+.site-footer a:focus {
     color: var(--clr-footer-link-hover)
 }
 </style>
