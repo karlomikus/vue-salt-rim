7d0f3595ceb4c2358c9987c1b63c37ea2a33b037
Karlo Mikuš <karlo.mikus1@gmail.com>
2024-11-01 21:08:19 +0100

    Rename var

--- src/UnitHandler.js
+++ src/UnitHandler.js
@@ -11,10 +11,10 @@ const handler = {
      *
      * @param {object} ingredient
      * @param {string} convertTo
-     * @param {number} servings
+     * @param {number} scaleFactor
      * @returns {string}
      */
-    print(ingredient, convertTo = 'ml', servings = 1) {
+    print(ingredient, convertTo = 'ml', scaleFactor = 1) {
         let orgAmount = 0
         if (ingredient.amount !== null && ingredient.amount !== undefined) {
             orgAmount = numericQuantity(`${ingredient.amount}`)
@@ -25,8 +25,8 @@ const handler = {
             orgAmountMax = numericQuantity(`${(ingredient.amount_max || 0)}`)
         }
 
-        orgAmount *= servings
-        orgAmountMax *= servings
+        orgAmount *= scaleFactor
+        orgAmountMax *= scaleFactor
 
         let orgUnits = ingredient.units ? ingredient.units.toLowerCase() : ''
 
