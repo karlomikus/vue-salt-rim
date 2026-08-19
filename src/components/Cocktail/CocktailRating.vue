197a2ba13663115ceee5881f196af331c754c1bb
Karlo Mikus <contact@karlomikus.com>
2026-08-19 22:10:58 +0200

    Add half star rating presentation

--- src/components/Cocktail/CocktailRating.vue
+++ src/components/Cocktail/CocktailRating.vue
@@ -1,29 +1,35 @@
 <template>
     <div class="resource-rating">
-        <template v-for="val in 5" :key="val">
-            <template v-if="val > (averageRating ?? 0)">
-                <svg class="resource-rating__empty" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                    <path
-                        d="M12.0006 18.26L4.94715 22.2082L6.52248 14.2799L0.587891 8.7918L8.61493 7.84006L12.0006 0.5L15.3862 7.84006L23.4132 8.7918L17.4787 14.2799L19.054 22.2082L12.0006 18.26ZM12.0006 15.968L16.2473 18.3451L15.2988 13.5717L18.8719 10.2674L14.039 9.69434L12.0006 5.27502L9.96214 9.69434L5.12921 10.2674L8.70231 13.5717L7.75383 18.3451L12.0006 15.968Z"
-                    ></path>
-                </svg>
-            </template>
-            <template v-else>
-                <svg class="resource-rating__full" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+        <span v-for="val in 5" :key="val" class="resource-rating__star" aria-hidden="true">
+            <svg class="resource-rating__empty" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                <path
+                    d="M12.0006 18.26L4.94715 22.2082L6.52248 14.2799L0.587891 8.7918L8.61493 7.84006L12.0006 0.5L15.3862 7.84006L23.4132 8.7918L17.4787 14.2799L19.054 22.2082L12.0006 18.26ZM12.0006 15.968L16.2473 18.3451L15.2988 13.5717L18.8719 10.2674L14.039 9.69434L12.0006 5.27502L9.96214 9.69434L5.12921 10.2674L8.70231 13.5717L7.75383 18.3451L12.0006 15.968Z"
+                ></path>
+            </svg>
+            <span class="resource-rating__filled" :style="{ width: `${fillPercent(val)}%` }">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                     <path
                         d="M12.0006 18.26L4.94715 22.2082L6.52248 14.2799L0.587891 8.7918L8.61493 7.84006L12.0006 0.5L15.3862 7.84006L23.4132 8.7918L17.4787 14.2799L19.054 22.2082L12.0006 18.26Z"
                     ></path>
                 </svg>
-            </template>
-        </template>
+            </span>
+        </span>
         <span v-if="userRating" class="resource-rating__alt" :title="$t('your-rating')">{{ userRating }}</span>
     </div>
 </template>
 <script setup lang="ts">
-withDefaults(defineProps<{ userRating?: number | null; averageRating?: number | null }>(), {
+import { computed } from "vue";
+
+const props = withDefaults(defineProps<{ userRating?: number | null; averageRating?: number | null }>(), {
     userRating: null,
     averageRating: null,
 });
+
+const averageRating = computed(() => props.averageRating ?? 0);
+
+function fillPercent(val: number): number {
+    return Math.max(0, Math.min(100, (averageRating.value - (val - 1)) * 100));
+}
 </script>
 <style scoped>
 .resource-rating {
@@ -33,10 +39,36 @@ withDefaults(defineProps<{ userRating?: number | null; averageRating?: number | 
     justify-content: center;
 }
 
-.resource-rating svg {
-    fill: var(--clr-rating);
+.resource-rating__star {
+    position: relative;
+    display: inline-block;
     width: 16px;
     height: 16px;
+}
+
+.resource-rating__star svg {
+    position: absolute;
+    top: 0;
+    left: 0;
+    fill: var(--clr-rating);
+    height: 100%;
+}
+
+.resource-rating__empty {
+    width: 100%;
+    opacity: 0.75;
+}
+
+.resource-rating__filled {
+    position: absolute;
+    inset: 0 auto 0 0;
+    display: block;
+    overflow: hidden;
+    height: 100%;
+}
+
+.resource-rating__filled svg {
+    width: 16px;
 }
 
 .resource-rating__alt {
@@ -50,9 +82,13 @@ withDefaults(defineProps<{ userRating?: number | null; averageRating?: number | 
 }
 
 @media (max-width: 450px) {
-    .resource-rating svg {
+    .resource-rating__star {
         width: 12px;
         height: 12px;
+    }
+
+    .resource-rating__filled svg {
+        width: 12px;
     }
 
     .resource-rating__alt {
